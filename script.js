ce4e354b57411183b8d8f9d0caef8a9cde455a56
Ian Raburu <williamraburu@gmail.com>
2023-07-05 19:02:13 +0300

    Wait for document to load then set initial variables

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('header.hero');
+    const section = document.querySelector('section.features');
+    const getStartedButton = document.querySelector('.hero-button');
+    const footer = document.querySelector('.footer');
+
+    
+});
