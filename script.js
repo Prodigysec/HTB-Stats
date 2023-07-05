e74ee2bbc82f60ccf5eb793b8cc216b69dfacce3
Ian Raburu <williamraburu@gmail.com>
2023-07-05 19:04:03 +0300

    Add an eventlistener to the button

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const getStartedButton = document.querySelector('.hero-button');
     const footer = document.querySelector('.footer');
 
-    
+    // Perform API request asyncronously
+
+    // Fetch data on page load
+
+    // Add eventlistener to button
+    getStartedButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // Remove the header, section and footer
+        header.remove();
+        section.remove();
+        footer.remove();
+    });
 });
